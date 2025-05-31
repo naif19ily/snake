263946e9f3251667f72ea0688fa2f05ac68fece8
naif19ily <juan.patino.dc@gmail.com>
2025-05-31 15:20:30 -0500

    getting terminal dimensions

--- err.asm
+++
@@ -1,8 +0,0 @@
-.section .text
-
-.include "macros.inc"
-
-.globl err_small_window
-
-err_small_window:
-	EX	$-1
---
+++ source/main.asm
@@ -0,0 +1,58 @@
+#                  _        
+#  ___ _ __   __ _| | _____ 
+# / __| '_ \ / _` | |/ / _ \
+# \__ \ | | | (_| |   <  __/
+# |___/_| |_|\__,_|_|\_\___|
+
+.section .rodata
+	#
+	# Defines the minimum dimensions to run
+	# the program properly
+	#
+	.MinRows: .word 110
+	.MinCols: .word 56
+
+
+.section .bss
+	# struct winsize
+	# {
+	#   unsigned short int ws_row;
+	#   unsigned short int ws_col;
+	#   unsigned short int ws_xpixel;
+	#   unsigned short int ws_ypixel;
+	# };
+	.TermSize: .zero 8
+
+.section .text
+
+.include "macros.inc"
+
+.globl _start
+
+_start:
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+	call	._getermsz
+
+	EXIT	$0
+
+._getermsz:	
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movq	$16, %rax
+	movq	$0, %rdi
+	movq	$21523, %rsi
+	leaq	.TermSize(%rip), %rdx
+	syscall
+	movw	(.TermSize), %ax
+	cmpw	.MinRows(%rip), %ax
+	jl	.fatal_dimns
+	movw	(.TermSize + 2), %ax
+	cmpw	.MinCols(%rip), %ax
+	jl	.fatal_dimns
+	leave
+	ret
+
+.fatal_dimns:
+	EXIT	$-1
